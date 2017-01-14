20edcc0a98a29927148b129c434282fe44ba3578
Jacob Hackett <jacobrhackett@gmail.com>
2017-01-14 16:33:42 -0500

    cleaning up files we're no longer using

--- tests.py
+++ tests.py
@@ -43,5 +43,22 @@ def test_cycle_heat():
 				toaster.enable_outside()
 		diff = time.time() - start_time
 
+def make_localtime(tstr):
+    """Convert HH:MM in UTC to HH:MM in local timezone."""
+    # hours west of UTC
+    offset_h = time.timezone // 3600 # hours west of UTC
+    # subtract offset, reduce mod24, cast to str
+    local_h = str((int(tstr[:2]) - offset_h + 24) % 24)
+    # get minutes
+    local_m = tstr[3:]
+    return "{}:{}".format(local_h, local_m)
+
+def test_time_conversion():
+	t_old = "17:30"
+	t_new = make_localtime(t_old)
+
+	print("UTC time\t{}".format(t_old))
+	print("Local time\t{}".format(t_new))
+
 if __name__ == "__main__":
     pass
